12a07f68d8e145deecb408d7a11133a7b4475e63
Milo Lee <milocarlos90@gmail.com>
2026-03-11 16:40:05 +0000

    - added first stg and assertions model

---
+++ definitions/sources/thelook_ecommerce.js
@@ -0,0 +1,11 @@
+declare({
+  database: "bigquery-public-data",
+  schema: "thelook_ecommerce",
+  name: "orders"
+});
+
+declare({
+  database: "bigquery-public-data",
+  schema: "thelook_ecommerce",
+  name: "users"
+});
